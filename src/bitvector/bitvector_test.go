650acb6820fec47f797ed25a0dabc67d3f47edca
Caleb Thorsteinson <caleb@thorsteinson.io>
2019-08-13 11:33:41 -0700

    Applies gofmt; Creates static test for adding

--- src/bitvector/bitvector_test.go
+++ src/bitvector/bitvector_test.go
@@ -1,13 +1,31 @@
 package bitvector
 
 import (
-	"testing"
 	"math/rand"
+	"testing"
 	"time"
 )
 
 const maxTestingSize = 10000
 const testingRounds = 100
+
+func Test_Add(t *testing.T) {
+	v := MakeVector(1000)
+
+	var tests = []int{0, 1, 2, 3, 4, 5, 6, 63, 64, 65, 127, 128, 129}
+
+	for _, n := range tests {
+		v.Add(n)
+		if !v.Contains(n) {
+			t.Errorf("Failed to add value %q", n)
+		}
+
+		v.Add(n)
+		if !v.Contains(n) {
+			t.Errorf("Failed idempotence test adding %q", n)
+		}
+	}
+}
 
 func TestAdd(t *testing.T) {
 	seed := time.Now().UnixNano()
@@ -26,14 +44,6 @@ func TestAdd(t *testing.T) {
 	}
 }
 
-// A really good test idea: We should add a ton of random values. We
-// use the values function and ensure that all the values were in fact
-// added. Then we call remove on all the same values and ensure that
-// after every item has been removed, nothing is contained. This is a
-// single comprehensive property test that can be run many times. Much
-// better to use this, and then save the basic manual cases for our
-// simpler tests
-
 func TestRemove(t *testing.T) {
 	bv := MakeVector(10)
 
@@ -45,18 +55,18 @@ func TestRemove(t *testing.T) {
 	}
 }
 
-func Test_Values (t *testing.T) {
+func Test_Values(t *testing.T) {
 	bv := MakeVector(10)
 
 	bv.Add(1)
 	bv.Add(2)
 	bv.Add(3)
 
-	resultSlice := []int{1,2,3}
+	resultSlice := []int{1, 2, 3}
 
 	equal := true
 
-	for i,x := range bv.Values() {
+	for i, x := range bv.Values() {
 		if x != resultSlice[i] {
 			equal = false
 			break
